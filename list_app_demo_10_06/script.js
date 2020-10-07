7481d9b39608b6295d642a10393e0830d4829002
tim tucker <timothytucker@berkeley.edu>
2020-10-06 21:11:12 -0700

    fixed bug, case error

--- list_app_demo_10_06/script.js
+++ list_app_demo_10_06/script.js
@@ -1,10 +1,9 @@
-const button = document.getElementByID('add-task-button')
-const input = document.getElementById('task-input')
-const list = document.getElementbyId('task-list')
+const button = document.getElementById("add-task-button")
+const input = document.getElementById("task-input")
+const list = document.getElementById("task-list")
 
 function addTask() {
   const task = input.value
-
   const listItem = document.createElement('li')
   listItem.textContent = task
   list.appendChild(listItem)
